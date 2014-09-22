c6be39993ace6c82e69a40f0a1bb5baaa6d47a11
Eric Vass <ericrvass@gmail.com>
2014-09-22 11:00:34 -0400

    Fixing fontawesome

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,48 @@ module Cadillac
     # config.i18n.default_locale = :de
     
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
+    
+    config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js|map)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    end
+     config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js|map)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('vendor', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    end
+    config.assets.paths << "#{Rails.root}/app/assets/fonts" 
+    config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
+    config.assets.paths << "#{Rails.root}/app/assets/images" 
+    config.assets.paths << "#{Rails.root}/vendor/assets/images"
+    config.assets.precompile += ["glyphicons-halflings-regular.ttf",
+                         "glyphicons-halflings-regular.eot",
+                         "glyphicons-halflings-regular.svg",
+                         "glyphicons-halflings-regular.woff"]
+    config.assets.precompile += ["fontawesome-webfont.ttf",
+                         "fontawesome-webfont.eot",
+                         "fontawesome-webfont.svg",
+                         "fontawesome-webfont.woff"]
   end
 end
